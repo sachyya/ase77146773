151f712d04ea54013109482dda2010e5d933901d
sachyya <sachyya1@gmail.com>
2015-12-07 00:03:23 +0545

    Fractal display only

---
+++ ASE77146773/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ASE77146773
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.animateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem,
+            this.animateToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.viewToolStripMenuItem.Text = "Action";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // animateToolStripMenuItem
+            // 
+            this.animateToolStripMenuItem.Name = "animateToolStripMenuItem";
+            this.animateToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.animateToolStripMenuItem.Text = "Color Palate";
+            this.animateToolStripMenuItem.Click += new System.EventHandler(this.animateToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 479);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem animateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ ASE77146773/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ASE77146773
+{
+    public partial class Form1 : Form
+    {
+        private const int MAX = 256;      // max iterations
+        private const double SX = -2.025; // start value real
+        private const double SY = -1.125; // start value imaginary
+        private const double EX = 0.6;    // end value real
+        private const double EY = 1.125;  // end value imaginary
+        private static int x1, y1, xs, ys, xe, ye;
+        private static double xstart, ystart, xende, yende, xzoom, yzoom;
+        private static bool action, rectangle, finished, colorChange;
+        private static float xy;
+        //private Image picture;
+        private Graphics g1;
+        private Bitmap picture;
+        Rectangle rec = new Rectangle(0, 0, 0, 0);
+        public HSBColor HSBcol = new HSBColor();
+        Graphics windowG;
+        bool mousePressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+            start();
+            menuStrip1.Cursor = Cursors.Hand;
+            //Added to reduce flickering
+            this.DoubleBuffered = true;
+            finished = true;
+        }
+
+
+
+
+        private void initvalues() // reset start values
+        {
+            xstart = SX;
+            ystart = SY;
+            xende = EX;
+            yende = EY;
+            if ((float)((xende - xstart) / (yende - ystart)) != xy)
+                xstart = xende - (yende - ystart) * (double)xy;
+
+        }
+
+       
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            init();
+            start();
+        }
+
+
+
+
+        public struct HSBColor
+        {
+            float h;
+            float s;
+            float b;
+            int a;
+
+            public HSBColor(float h, float s, float b)
+            {
+                this.a = 0xff;
+                this.h = Math.Min(Math.Max(h, 0), 255);
+                this.s = Math.Min(Math.Max(s, 0), 255);
+                this.b = Math.Min(Math.Max(b, 0), 255);
+            }
+            public HSBColor(int a, float h, float s, float b)
+            {
+                this.a = a;
+                this.h = Math.Min(Math.Max(h, 0), 255);
+                this.s = Math.Min(Math.Max(s, 0), 255);
+                this.b = Math.Min(Math.Max(b, 0), 255);
+            }
+
+
+            public static Color FromHSB(HSBColor hsbColor)
+            {
+                float r = hsbColor.b;
+                float g = hsbColor.b;
+                float b = hsbColor.b;
+                if (hsbColor.s != 0)
+                {
+                    float max = hsbColor.b;
+                    float dif = hsbColor.b * hsbColor.s / 255f;
+                    float min = hsbColor.b - dif;
+
+                    float h = hsbColor.h * 360f / 255f;
+
+                    if (h < 60f)
+                    {
+                        r = max;
+                        g = h * dif / 60f + min;
+                        b = min;
+                    }
+                    else if (h < 120f)
+                    {
+                        r = -(h - 120f) * dif / 60f + min;
+                        g = max;
+                        b = min;
+                    }
+                    else if (h < 180f)
+                    {
+                        r = min;
+                        g = max;
+                        b = (h - 120f) * dif / 60f + min;
+                    }
+                    else if (h < 240f)
+                    {
+                        r = min;
+                        g = -(h - 240f) * dif / 60f + min;
+                        b = max;
+                    }
+                    else if (h < 300f)
+                    {
+                        r = (h - 240f) * dif / 60f + min;
+                        g = min;
+                        b = max;
+                    }
+                    else if (h <= 360f)
+                    {
+                        r = max;
+                        g = min;
+                        b = -(h - 360f) * dif / 60 + min;
+                    }
+                    else
+                    {
+                        r = 0;
+                        g = 0;
+                        b = 0;
+                    }
+                }
+
+                if (colorChange == true)
+                {
+                    return Color.FromArgb
+                    (
+                        (int)60,
+                        (int)Math.Round(Math.Min(Math.Max(g, 0), 255)),
+                        (int)Math.Round(Math.Min(Math.Max(b, 0), 255))
+                     );
+                }
+
+                return Color.FromArgb
+                    (
+                        hsbColor.a,
+                        (int)Math.Round(Math.Min(Math.Max(r, 0), 255)),
+                        (int)Math.Round(Math.Min(Math.Max(g, 0), 255)),
+                        (int)Math.Round(Math.Min(Math.Max(b, 0), 255))
+                     );
+            }
+
+        }
+
+        
+
+        private float pointcolour(double xwert, double ywert) // color value from 0.0 to 1.0 by iterations
+        {
+            double r = 0.0, i = 0.0, m = 0.0;
+            int j = 0;
+
+            while ((j < MAX) && (m < 4.0))
+            {
+                j++;
+                m = r * r - i * i;
+                i = 2.0 * r * i + ywert;
+                r = m + xwert;
+            }
+
+            return (float)j / (float)MAX;
+        }
+
+
+
+        public void init() // all instances will be prepared
+        {
+            finished = false;
+            x1 = 640;
+            y1 = 480;
+            xy = (float)x1 / (float)y1;
+            picture = new Bitmap(x1, y1);
+            g1 = Graphics.FromImage(picture);
+        }
+
+       
+
+        public void start()
+        {
+            action = false;
+            rectangle = false;
+            initvalues();
+            xzoom = (xende - xstart) / (double)x1;
+            yzoom = (yende - ystart) / (double)y1;
+            Pen p = new Pen(Color.Red, 2);
+            mandelbrot();
+        }
+
+        public void stop()
+        {
+        }
+
+        public void paint(Graphics g)
+        {
+            update(g);
+        }
+
+        public void update(Graphics g)
+        {
+
+        }
+
+        private void mandelbrot()
+        {
+            int x, y;
+            float h, b, alt = 0.0f;
+            Pen p = new Pen(Color.Red, 2); //changed
+            action = false;
+            this.Cursor = Cursors.WaitCursor;
+            for (x = 0; x < x1; x += 2)
+                for (y = 0; y < y1; y++)
+                {
+                    h = pointcolour(xstart + xzoom * (double)x, ystart + yzoom * (double)y); // color value
+                    if (h != alt)
+                    {
+                        alt = h;
+                    }
+                    b = 1.0f - h * h; // brightness
+
+                    Color col = HSBColor.FromHSB(new HSBColor(h * 255, 0.8f * 255, b * 255));
+                    p = new Pen(col);
+                    g1.DrawLine(p, x, y, x + 1, y);
+                }
+            this.Cursor = Cursors.Cross;
+            action = true;
+        }
+
+        //for colour cycling
+        public Color FromHsv(double hue, double saturation, double value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
+        }
+
+    }
+
+}
